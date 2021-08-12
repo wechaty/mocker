6d0390c3382f9e983fcde0a16bf46a18034a32f0
Huan <zixia@zixia.net>
2021-08-12 13:14:39 +0800

    fix bot message event lost (https://github.com/wechaty/wechaty/issues/2230)

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -2,10 +2,20 @@
 
 import { test }  from 'tstest'
 
+import { Message } from 'wechaty'
+
 import { createFixture } from '../src/mod'
 
 test('integration testing', async t => {
   for await (const fixture of createFixture()) {
-    t.ok(fixture, 'should instantiate wechaty with puppet mocker')
+    const bot = fixture.wechaty.wechaty.userSelf()
+
+    const directMessage = await new Promise<Message>(resolve => {
+      bot.once('message', resolve)
+      fixture.mocker.player.say().to(fixture.mocker.bot)
+    })
+    fixture.mocker.player.say('test').to(fixture.mocker.bot)
+
+    t.ok(directMessage, 'should resolve a message')
   }
 })
